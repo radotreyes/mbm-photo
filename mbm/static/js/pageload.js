8f36666001c0c39b54418b52c10beee8f4b1809b
Reuben Reyes <reuben.a.reyes@gmail.com>
2017-07-17 22:53:02 -0700

    small css changes to gallery, add modals

---
+++ mbm/static/js/pageload.js
@@ -0,0 +1,9 @@
+// $( window ).load( function() {
+//   var images = [];
+//   console.log( document.images
+//   for( let image in document.images ) {
+//     images.push( image );
+//   }
+//
+//   console.log( images );
+// } );
