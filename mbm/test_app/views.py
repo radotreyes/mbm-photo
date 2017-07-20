ba629867be267d357370c5874bf8d973f86beb98
radotreyes <reuben.a.reyes@gmail.com>
2017-07-20 04:47:52 +0000

    production cleanup

--- mbm/test_app/views.py
+++ mbm/test_app/views.py
@@ -26,7 +26,7 @@ class Login( FormView ):
     template_name = 'test_app/login/index.html'
     form_class = AuthenticationForm
     redirect_field_name = REDIRECT_FIELD_NAME
-    success_url = settings.LOGIN_REDIRECT_URL
+    success_url = reverse_lazy( 'test_app:index.html' ).
 
     def get_context_data( self, **kwargs ):
         title = self.title
